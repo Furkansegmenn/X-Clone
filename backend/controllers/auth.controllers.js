0c4404434d5bf95f90316aadb1794ef2baa1403b
Furkan <segmenfurkann@gmail.com>
2024-10-18 16:38:20 +0300

    1- project setup

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,9 @@
+export const signup = async (req, res) => {
+	res.json({ data: "in signup func" });
+};
+export const login = async (req, res) => {
+	res.json({ data: "in login func" });
+};
+export const logout = async (req, res) => {
+	res.json({ data: "in logout func" });
+};
