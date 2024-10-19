642b6f211018a5ee8063f790f6d3267dea5cf23d
Furkan <segmenfurkann@gmail.com>
2024-10-20 01:58:13 +0300

    1- post routes and controllers added

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,13 +1,24 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost, deletePost } from "../controllers/post.controllers.js";
+import {
+	commentOnPost,
+	createPost,
+	deletePost,
+	getAllPosts,
+	getFollowingPosts,
+	getLikedPosts,
+	getUserPosts,
+	likeUnlikePost,
+} from "../controllers/post.controllers.js";
 
 const router = express.Router();
-
+router.get("/", protectRoute, getAllPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
 router.post("/create", protectRoute, createPost);
-// router.post("/like/:id", protectRoute, likeUnlikePost);
-// router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
-// router.update("/", protectRoute, updatePost);
 
 export default router;
