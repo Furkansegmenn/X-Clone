e17d3e8f2972039d53debcad8ff2307fad795627
Furkan <segmenfurkann@gmail.com>
2024-10-19 18:24:12 +0300

    1- user routes and controllers added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { followUnfollowUser, getUserProfile } from "../controllers/user.controllers.js";
+import {
+	followUnfollowUser,
+	getSuggestedUsers,
+	getUserProfile,
+	updateUserProfile,
+} from "../controllers/user.controllers.js";
 
 const router = express.Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile); //burda token kontrolü yapmalı mıyız bilmiyorum ?
-// router.get("/suggested", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/follow/:id", protectRoute, followUnfollowUser);
-// router.post("/update", protectRoute, updateUserProfile);
+router.post("/update", protectRoute, updateUserProfile);
 
 export default router;
